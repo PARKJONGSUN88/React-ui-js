365c588d8f8f85287d0b638fe32bb954e1dafc5f
PARK JONG SUN <50945715+PARKJONGSUN88@users.noreply.github.com>
2020-07-25 01:00:02 +0900

    commit

--- Typescript_React/src/pages/testPage.tsx
+++ Typescript_React/src/pages/testPage.tsx
@@ -1,28 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
 import SpeedDial from '../components/SpeedDial';
-import Quickmenu from '../img/quickmenu.png';
-import Facebook from '../img/facebook.png';
-import Insta from '../img/instagram.png';
-import Twitter from '../img/twitter.png';
-import Youtube from '../img/youtube.png';
+import mainMenu from '../img/quickmenu.png';
+import facebook from '../img/facebook.png';
+import insta from '../img/instagram.png';
+import twitter from '../img/twitter.png';
+import youtube from '../img/youtube.png';
 
 const Cover = styled.div`
   position: fixed;
-  top: 300px;
-  left: 300px;
+  top: 150px;
+  left: 100px;
 `;
 
 const testPage = () => {
   return (
     <Cover>
       <SpeedDial
-        button={<img src={Quickmenu} alt="" width="100%" />}
+        button={<img src={mainMenu} alt="" width="100%" />}
+        fDeg={0}
+        deg={-45}
+        between={100}
         dials={[
-          { icon: <img src={Facebook} alt="" width="100%" /> },
-          { url: false, icon: <img src={Insta} alt="" width="100%" /> },
-          { url: 1, icon: <img src={Twitter} alt="" width="100%" /> },
-          { url: '유튜브', icon: <img src={Youtube} alt="" width="100%" /> },
+          { icon: <img src={facebook} alt="" width="100%" /> },
+          { url: false, icon: <img src={insta} alt="" width="100%" /> },
+          { url: 1, icon: <img src={twitter} alt="" width="100%" /> },
+          { url: '유튜브', icon: <img src={youtube} alt="" width="100%" /> },
         ]}
       />
     </Cover>
